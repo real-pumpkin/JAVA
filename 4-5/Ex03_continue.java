f5eecfd0ab61ef79b80c61bc01f0e09f6c339ec4
real-pumpkin <164877530+real-pumpkin@users.noreply.github.com>
2024-08-16 21:04:03 +0900

    Add files via upload

---
+++ 4-5/Ex03_continue.java
@@ -0,0 +1,30 @@
+package ch04.unit05;
+
+public class Ex03_continue {
+
+	public static void main(String[] args) {
+
+		/*
+		for (int i = 1; i <= 3; i++) {
+			for (int j = 1; j <= 3; j++) {
+				if (i + j == 4) {
+					continue;
+				}
+				System.out.println("i:" + i + ", j:" + j);
+			}
+		}
+		*/
+
+		gogo:
+		for (int i = 1; i <= 3; i++) {
+			for (int j = 1; j <= 3; j++) {
+				if (i + j == 4) {
+					continue gogo;
+				}
+				System.out.println("i:" + i + ", j:" + j);
+			}
+		}
+		
+	}
+
+}
