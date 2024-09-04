fa064e36d0f3cee29cd60c6473ca41a43b2b4e16
real-pumpkin <164877530+real-pumpkin@users.noreply.github.com>
2024-09-04 22:55:59 +0900

    Add files via upload

---
+++ Exaddr/AddressVO.java
@@ -0,0 +1,46 @@
+package ch12.unit02.addr2;
+
+public class AddressVO {
+	private String name;
+	private String birth;
+	private String tel;
+	private String addr;
+	private String regDate;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	
+	@Override
+	public String toString() {
+		String s=name+"\t"+birth+"\t"+tel+"\t"+addr+"\t"+regDate;
+		return s;
+	}
+}
---
+++ Exaddr/App.java
@@ -0,0 +1,8 @@
+package ch12.unit02.addr2;
+
+public class App {
+	public static void main(String[] args) {
+		AddressUI ui = new AddressUI();
+		ui.menu();
+	}
+}
